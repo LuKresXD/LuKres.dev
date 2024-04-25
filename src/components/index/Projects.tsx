a4ab77f9400e2c684c124556ebcf18f444d14714
lukres <lukres006@gmail.com>
2024-04-25 17:09:08 +0300

    fixed?

--- src/components/index/Projects.tsx
+++ src/components/index/Projects.tsx
@@ -16,18 +16,18 @@ export default function Projects() {
         </motion.h1>
         <ul className="grid sm:grid-cols-2 grid-cols-1 gap-4 mt-4">
           <ProjectCard
-            url="https://lukres.dev/payloads"
+            url="https://www.lukres.dev/payloads"
             title="RubberDucky Payloads"
             description="All scripts created by me or partially by me in RubberDucky language are stored here. I tested them on Flipper Zero. For educational purposes"
-            image="https://cdn.discordapp.com/attachments/1225694526484643914/1233051073720352918/Z7KSHodItHk5UKKCgmWdP_badusb1_negate.png?ex=662bafcc&is=662a5e4c&hm=5897781e254de1083c02a6d7fa6d8783e04c0d20c75533482a0e0ee8a4fb2ceb&"
+            image="https://cdn.discordapp.com/attachments/1225694526484643914/1233054878985879644/Z7KSHodItHk5UKKCgmWdP_badusb1_ne.png?ex=662bb358&is=662a61d8&hm=af748584198a7e5f8b460fe609bc488d98fb11425872d0b9c50b092e83ad6119&"
             delay={0.1}
             gradient="bg-gradient-to-br"
           />
           <ProjectCard
-            url="https://google.com"
-            title="Google"
-            description="Desciption"
-            image="https://media.discordapp.net/attachments/1225694526484643914/1232949355464359947/luminal_logo.jpg?ex=662b5111&is=6629ff91&hm=f4452cc2df53b9ef383cdd00430b6d7cde20298b56602c18fcafb54ce6acb7ca&=&format=webp&width=1011&height=1011"
+            url="https://www.lukres.dev/home"
+            title="LuKres.dev"
+            description="This page :)"
+            image="https://cdn.discordapp.com/attachments/1225694526484643914/1233057010178719804/CleanShot_2024-04-25_at_17.04.232x.png?ex=662bb554&is=662a63d4&hm=51ef0ac6885d78771ad8a420d18bc7a89fe4a4b899bba273875619c4633fea06&"
             delay={0.2}
             gradient="bg-gradient-to-bl"
           />
